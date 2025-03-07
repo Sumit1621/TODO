c3706f7ec22bb695cde6b1abc3b3982ccfc31a98
saumya <saumyajena2004@gmail.com>
2025-03-07 22:47:53 +0530

    update html

--- Todo/views.py
+++ Todo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponseRedirect
+from django .urls import reverse
 
 # Create your views here.
+
+def update(request, pk):
+    todo = Todo.objects.get(pk=pk)
+    d = {'todo': todo}
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        desc = request.POST.get('desc')
+        todo.title = title
+        todo.desc = desc
+        todo.save()
+        return HttpResponseRedirect(reverse('home'))
+    return render(request,'update.html',d)
